af36646f5cdda3daf68938ab994a587f17f12350
dhwkdal5522 <dhwkdal5522@gmail.com>
2025-10-10 14:36:43 +0900

    .

--- test/js/menu_251002.js
+++ test/js/menu_251002.js
@@ -1,5 +1,28 @@
 
 $(document).ready(function(){
+    /**
+     * 기준 1024이하면 모바일 1025 이상이면 pc 
+     * 브라우저의 넓이에 따라서 지금 pc 모드인지 모바일인지 구분
+     */
+
+    let mobile_size = 1024
+    let device_status // pc 인지 mobile인지 구분 
+    let win_w // 브라우저 넓이
+
+    function size_chk(){ //함수 정의
+        win_w = $(window).width()
+        if(win_w > mobile_size){
+            device_status = 'pc'
+        }else{
+            device_status = 'mobile'
+        }
+        console.log(device_status)
+    }
+    size_chk() //함수 호출 (문서가 로딩된 이후 1번 실행)
+    $(window).resize(function(){//브라우저가 리사이즈 될 때 마다 1번 실행 
+        size_chk() //함수 호출 
+    })
+
 /**누구한테 header .gnb .gnb_wrap ul.depth1
  * 뭘 했을때 오버했을 때
  * 결론
@@ -38,6 +61,15 @@ $('header .util .lang .lang_open').on('focusin',function(){
 })
 /** 모바일에서 1차메뉴를 클릭하면 2차 메뉴 
  */
-
-
+	$('header .gnb .gnb_wrap ul.depth1 > li > a').on('click', function(e){
+		//e.preventDefault();		/* a 태그의 href를 작동 시키지 않음 */
+	});
+    
+    $('header .gnb .gnb_wrap ul.depth1 > li > a').on('click', function(e){
+        if(device_status == 'mobile'){
+            e.preventDefault();		/* a 태그의 href를 작동 시키지 않음 */
+            console.log('모바일이다!!!')
+        }
+    }); 
+   
 })/* */
