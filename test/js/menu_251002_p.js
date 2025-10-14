eef7b6d2e65d115d7b6d9ea2087d9c93644eaa4c
dhwkdal5522 <dhwkdal5522@gmail.com>
2025-10-15 00:56:41 +0900

    .

---
+++ test/js/menu_251002_p.js
@@ -0,0 +1,11 @@
+// $(document).ready(function(){
+//     $('header .gnb').on('mouseenter',function(){
+//         //console.log('어예~')
+//     $('header').addClass('menu_over')
+//     })
+//     $('header .gnb').on('mouseleave',function(){
+//        // console.log('시무룩')
+//     $('header').removeClass('menu_over')
+//     })
+
+// })//
