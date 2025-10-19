2c1dbefc0a6e5b854ff42f2fe5cf93c47e32aeb9
dhwkdal5522 <dhwkdal5522@gmail.com>
2025-10-19 23:51:25 +0900

    .

--- animal/js/main.js
+++ animal/js/main.js
@@ -168,4 +168,40 @@ $(document).ready(function(){
     }) 
     //test 할때는 html 에서 fixed를 지워야한다. 
     /****************** 끝 : 스크롤시 header에 fixed ****************/
+     /****************** 시작 : 찾습니다 swiper ****************/
+     const find1_swiper = new Swiper('.find item1 .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            640: {    /* 640px 이상일때 적용 */
+                slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        navigation: {
+            nextEl: '.find item1 next',
+            prevEl: '.find item1 prev',
+        },
+        
+    });
+     const find2_swiper = new Swiper('.find item2 .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            640: {    /* 640px 이상일때 적용 */
+                slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        navigation: {
+            nextEl: '.find item2 next',
+            prevEl: '.find item2 prev',
+        },
+        
+    });
+     /****************** 끝 : 찾습니다 swiper ****************/
 })//
